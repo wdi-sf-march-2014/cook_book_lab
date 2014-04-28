cd46f572e6fbf4d51e0b5d55a9b38c7c56faa27d
tigarcia <tim.m.garcia@gmail.com>
2014-04-28 11:16:20 -0700

    Adding a cookbook starter app for the cookbook lab

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cookbook::Application.config.secret_key_base = '218a5ada6b013440322dc31bc83458f1e8d112fd276d4281bc27cbb7ebf8c724e59a570e2f82caa9049235e91bb9643ee9bc8c7307b8885bfa659c5ae133a85a'
