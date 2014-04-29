f86b9ce865ec8e076d5f5ec49b3b19cdf94284fb
taylorchiu <taylorparkerchiu@gmail.com>
2014-04-28 17:43:02 -0700

    Working App w Ingredient views

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,10 @@
+ingredients = Ingredient.create([
+	{name: "butter", brand: "LandoLakes", image: "http://cdn.gmoinside.org/wp-inside/uploads/2013/12/Land-O-Lakes.jpg"},
 
+	{name: "milk", brand: "Lucerne", image: "http://static.caloriecount.about.com/images/medium/lucerne-milk-fat-free-18026.jpg"},
 
-# TODO: Create your own seed data!
+	{name: "sugar", brand: "CH", image: "http://happymoneysaver.com/wp-content/uploads/2012/02/CH.jpg"},
+
+	{name: "flour", brand: "Gold medal", image: "http://www.meijer.com/assets/product_images/styles/xlarge/1001029_016000107106_A_400.jpg"}
+
+	])
