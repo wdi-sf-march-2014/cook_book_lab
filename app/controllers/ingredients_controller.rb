c0c64fd9f6bc5a4d4a1acd60a307f2a468d80bba
blairprophett <blaircuny@gmail.com>
2014-04-28 17:43:16 -0700

    working app with views, controller, etc

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,23 +1,39 @@
 class IngredientsController < ApplicationController
   
   def index
+    @ingredients = Ingredient.all
   end
 
   def new
+    @ingredient = Ingredient.new
   end
 
   def create
+    @ingredient = Ingredient.create ingredients_params
   end
 
   def show
+    @ingredient = Ingredient.find(params[:id])
   end
 
   def edit
+    @ingredient = Ingredient.find(params[:id])
   end
 
   def update
+    ingredient = Ingredient.find(params[:id])
+    ingredient.update_attributes ingredient_params
+    redirect_to(ingredient)
   end
 
   def destroy
+    ingredient = Ingredient.find(params[:id])
+    ingredient.delete
+    redirect_to(ingredients_path)
   end
+  
+  private
+    def ingredients_params
+      params.require(:ingredient).permit(:name, :brand, :image)
+    end
 end
