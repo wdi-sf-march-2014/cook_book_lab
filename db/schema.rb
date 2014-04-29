c18e7fb096f09ea687a8d3b4b2a868bd7db2c439
Henry Huang <hhuang2088@gmail.com>
2014-04-28 18:19:30 -0700

    created ingredient

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140428182950) do
+ActiveRecord::Schema.define(version: 20140429011647) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "ingredients", force: true do |t|
+    t.string   "name"
+    t.string   "brand"
+    t.string   "image"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "recipes", force: true do |t|
     t.string   "name"
